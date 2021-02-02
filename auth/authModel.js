a0a734533c8c1556d4593319ece47ec20417ddb7
Ken Ruf <ruf.kenichi@gmail.com>
2021-02-02 01:19:03 -0600

    troubleshooting

--- auth/authModel.js
+++ auth/authModel.js
@@ -17,8 +17,8 @@ const register = async (user,org) => {
 }
 
 const addOrgUser = async (userId, orgId) => {
-  const user_id = await userId
-  const org_id = await orgId
+  const user_id = userId
+  const org_id = orgId
   console.log(user_id, org_id)
   const orgUser = {
     user_id: user_id[0],
