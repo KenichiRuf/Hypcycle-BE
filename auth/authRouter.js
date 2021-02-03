7ef678f368a07496e195f5685e4e9ab1853a423b
Ken Ruf <ruf.kenichi@gmail.com>
2021-02-02 22:41:51 -0600

    add console.log to login

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -34,6 +34,7 @@ router.post("/login", async (req, res) => {
   let { email, password } = req.body;
   try {
     const user = await Users.findBy({ email });
+    console.log(user)
     if (user && bcrypt.compareSync(password, user.password)) {
       const token = genToken(user);
       res.status(200).json({
