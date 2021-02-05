b6360c9b6d93adb9794410bac9e68bbe8e61a731
Ken Ruf <ruf.kenichi@gmail.com>
2021-02-04 22:48:19 -0600

    updated invite endpoint

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -22,7 +22,6 @@ router.post("/register", verifyUniqueEmail, verifyUniqueOrgName, async (req, res
   try {
     const newUser = await Users.addUser(user)
     const newOrg = await Orgs.addOrg(org)
-    console.log(newUser, newOrg)
     const newOrgUser = await Auth.addOrgUser(newUser, newOrg)
     res.status(201).json({message: "Registration Successful", userId: newUser[0], orgId: newOrg[0], orgUserId: newOrgUser[0]})
   } catch(err) {
@@ -34,7 +33,6 @@ router.post("/login", async (req, res) => {
   let { email, password } = req.body;
   try {
     const user = await Users.findBy({ email });
-    console.log(user)
     if (user && bcrypt.compareSync(password, user.password)) {
       const token = genToken(user);
       res.status(200).json({
@@ -46,7 +44,6 @@ router.post("/login", async (req, res) => {
       res.status(401).json({ message: "Invalid Credentials" });
     }
   } catch (error) {
-    console.log(error)
     res.status(500).json({ message: "Error logging in user", error: error });
   }
 });
@@ -60,7 +57,7 @@ const genToken = user => {
   const secret = process.env.JWT_SECRET;
 
   const options = {
-    expiresIn: "1h"
+    expiresIn: "24h"
   };
 
   return jwt.sign(payload, secret, options);
